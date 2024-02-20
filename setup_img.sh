4678461da3d91ebffa8912a11184f2bb208d6468
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2024-02-20 18:19:54 +0000

    fix errors

--- setup_img.sh
+++ setup_img.sh
@@ -1,17 +1,26 @@
-#!/bin/sh
-cp app edk2/KlpsPkg
-cp target.txt edk2/Conf/target.txt
+#!/bin/bash
 
 cd edk2
 source edksetup.sh
 
+# Use -r option to recursively copy 'app' directory
+cp -r ../app/KlpsPkg
+cp ../target.txt Conf/target.txt
+
 rm -rf Build
 
-build # Don't forget to set TARGET in target.txt to KlpsPkg/KlpsPkg.dsc
+# in case
+make -C /workspaces/KalapagosOS/edk2/BaseTools/Source/C
+
+# Assuming 'build' is the correct build command, replace it accordingly if it's different
+build 
+
+# Don't forget to set TARGET in target.txt to KlpsPkg/KlpsPkg.dsc
 # OvmfPkg/OvmfPkgX64.dsc is for OVMF.fd
 cd ..
 
 src="edk2/Build/KlpsPkgX64/RELEASE_GCC5/X64/KlpsPkg/applications/src/main/OUTPUT/Main.efi"
 BOOTX64Path="img/EFI/BOOT/BOOTX64.sh"
 
-cp $src $BOOTX64ELFPath
+# Corrected the variable name and added destination file
+cp "$src" "$BOOTX64Path"
